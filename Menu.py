a23a006dd0dd8f2ab736a6318ec6599f269a8ca9
c-Teck <igeolatoye@yahoo.com>
2021-12-30 03:40:45 -0800

    Updated env file

--- Menu.py
+++ Menu.py
@@ -1,6 +1,10 @@
-from hash_maker import password
+#!/usr/bin/env python3
+
+#from hash_maker import password
 import subprocess
-from database_manager import store_passwords, find_users, find_password, password_gen,update_details
+#from database_manager.py import store_passwords, find_users, find_password,update_details
+from master_pwd.py import password_gen
+import database_manager.py
 
 def menu():
     print('-'*30)
@@ -16,26 +20,27 @@ def menu():
     return input(': ')
 
 def create():
-   print('Please proivide the name of the site or app you want to generate a password for')
-   app_name = input()
-   print('[+]Please provide a simple password for this site or leave empty to generate a secured password for you :')
-   if input() != "" :
+    print('Please proivide the name of the site or app you want to generate a password for')
+    app_name = input()
+    print('[+]Please provide a simple password for this site or leave empty to generate a secure password for you :')
+    if input() != "" :
        plaintext = input()
     else :
-        plain_text = password_gen(8)
-   passw = password(plaintext, app_name, 12)
-   subprocess.run('xclip', universal_newlines=True, input=passw)
-   print('-'*30)
-   print('')
-   print('Your password has now been created and copied to your clipboard')
-   print('')
-   print('-' *30)
-   user_email = input('Please provide a user email for this app or site')
-   username = input('Please provide a username for this app or site (if applicable)')
-   if username == None:
+        plaintext = password_gen(8)
+
+    passw = password(plaintext, app_name, 12)
+    subprocess.run('xclip', universal_newlines=True, input=passw)
+    print('-'*30)
+    print('')
+    print('Your password has now been created and copied to your clipboard')
+    print('')
+    print('-' *30)
+    user_email = input('Please provide a user email for this app or site')
+    username = input('Please provide a username for this app or site (if applicable)')
+    if username == None:
        username = ''
-   url = input('Please paste the url to the site that you are creating the password for')
-   store_passwords(passw, user_email, username, url, app_name)
+    url = input('Please paste the url to the site that you are creating the password for')
+    store_passwords(passw, user_email, username, url, app_name)
 
 def find():
    print('Please provide the name of the site or app you want to find the password to')
--- mainfunc.py
+++ mainfunc.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
 
 from secret import get_secret_key
 from Menu import menu, create, find, find_accounts, Update
-from database-manager import connect
+from database_manager import connect
+import vault_registeration.py
 
 # menu
 # 1. create new password for a site
@@ -9,30 +11,34 @@ from database-manager import connect
 # 3. Find all sites connected to an email
 
 secret = get_secret_key()
-
+terminate_buttons = ["Q", "Exit", "exit", "EXIT", "terminate", "stop"]
 passw = input('[+] Please provide the master password to start using your password vault manager : ')
 
 if passw == secret:
     print('>>>\n>>>\n>>>\n [+] Successfully Authenticated...')
 
 else:
-    print('no luck')
+    print('no luck, try again')
     exit()
 
 choice = menu()
-while choice != 'Q':
-    if choice == '1':
-        create()
-    if choice == '2':
-        find_accounts()
-    if choice == '3':
-        find()
-    if choice == '4' :
-        Update()
-    if choice == '6' :
-        print('[+] Relax we are working on this update soon...')
-    if choice == '7' :
-        print(' [+] This update will be rolled over soon....')
-    else:
-        choice = menu()
+while choice not in terminate_buttons:
+    try:
+        if choice == '1':
+            create()
+        if choice == '2':
+            find_accounts()
+        if choice == '3':
+            find()
+        if choice == '4' :
+            Update()
+        if choice == '6' :
+            print('[+] Relax we are working on this update soon...')
+        if choice == '7' :
+            print(' [+] This update will be rolled over soon....')
+        else:
+            choice = menu()
+
+    except Exception as e:
+        print("[-] Internal error occured. Contact the administrator...")
 exit()
