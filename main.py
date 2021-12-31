982038565555b1b06245cd50a48763d2b4d0bef3
c-Teck <igeolatoye@yahoo.com>
2021-12-30 16:35:56 -0800

    Updated database to request details from env

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 import os
-import json
 import sys
 import getpass
 from termcolor import colored
 from dotenv import load_dotenv
+import master_pwd
+import hashlib
 
 
-
+load_dotenv()
 def exit_program():
     print(colored("Exiting...", "red"))
     sys.exit()
 
 def main():
 
-    Two_Factor = input("[+] Enter your two factor authenticator here : ")
-
     master_password_input = getpass.getpass("Master Password: ").encode()
+    Two_Factor = input("[+] Enter your two factor authenticator here : ")
 
     second_FA_location = Two_Factor.encode()
     #second_FA_location = "Dee Boo Dah".encode()
 
     master_password_hash = hashlib.sha256(master_password_input + second_FA_location).hexdigest()
 
-    if master_password.query_master_pwd(master_password_input, second_FA_location) is True:
+    if master_pwd.query_master_pwd(master_password_input, second_FA_location) is True:
 
         connection = db_connect.connection_db()
 
--- master_pwd.py
+++ master_pwd.py
@@ -6,26 +6,27 @@ Created on Thu Sep 30 10:04:32 2021
 @author: cybree
 """
 
-#from hashlib import sha256
+# from hashlib import sha256
 from Cryptodome.Cipher import AES 
 from pbkdf2 import PBKDF2
 import hashlib
 from base64 import b64encode, b64decode
 from dotenv import load_dotenv
 import os
-#from password_gen_func import master_password_gen
+import string
+import secrets
 
 
-
-
+load_dotenv()
 # Enter salt here in ******* field. Enter binary string.
-salt = b'********'
+# salt = b'********'
 
-def Salt():
-    load_dotenv()
 
+def salt():
     try:
-
+        os.environ['SALT'] = secrets.token_hex(64)
+    except Exception as error:
+        print('ERROR', error)
 
 
 def password_gen(password_length):
@@ -36,29 +37,34 @@ def password_gen(password_length):
 
     return secure_password
 
+
 def master_password_gen():
-
+    # os.environ["SALT"] = salt().
     master_pwd = input("Enter your master password to use for your vault : ").encode()
+    two_factor = os.environ.get("SALT").encode()
+    compile_factor_together = hashlib.sha256(master_pwd + two_factor).hexdigest()
+    print("\n[+] Generating your hashed password...\n...\n[+] Your hashed password has been generated")
+    hashed = (str(compile_factor_together))
+    os.environ["MASTER"] = hashed
 
-    compile_factor_together = hashlib.sha256(master_pwd).hexdigest()
-    print("\n[+] Generating your hashed password...\n...\n[+] Your password has been generated")
 
-    return (str(compile_factor_together))
+def query_master_pwd(master_password, second_fa_location):
 
+    # Enter password hash in ******** field. Use PBKDF2 and Salt from above.
+    # Use master_password_hash_generator.py to generate a master password hash.
+    master_password_hash = os.environ.get("MASTER")
 
-def query_master_pwd(master_password, second_FA_location): 
-
-    # Enter password hash in ******** field. Use PBKDF2 and Salt from above. Use master_password_hash_generator.py to generate a master password hash.
-    master_password_hash = master_password_gen()
-
-    compile_factor_together = hashlib.sha256(master_password + second_FA_location).hexdigest()
+    compile_factor_together = hashlib.sha256(master_password + second_fa_location).hexdigest()
 
     if compile_factor_together == master_password_hash: 
-        return True 
-    
+        return True
+    else:
+        return False
+
+
 def encrypt_password(password_to_encrypt, master_password_hash): 
-    
-    key = PBKDF2(str(master_password_hash), salt).read(32)
+    salt_auth = os.environ.get('SALT')
+    key = PBKDF2(str(master_password_hash), salt_auth).read(32)
     
     data_convert = str.encode(password_to_encrypt)
 
@@ -74,15 +80,16 @@ def encrypt_password(password_to_encrypt, master_password_hash):
 
     return encoded_ciphertext
 
+
 def decrypt_password(password_to_decrypt, master_password_hash): 
     
     if len(password_to_decrypt) % 4:
      
-     password_to_decrypt += '=' * (4 - len(password_to_decrypt) % 4)
+        password_to_decrypt += '=' * (4 - len(password_to_decrypt) % 4)
 
     convert = b64decode(password_to_decrypt)
-
-    key = PBKDF2(str(master_password_hash), salt).read(32)
+    salt_auth = os.environ.get('SALT')
+    key = PBKDF2(str(master_password_hash), salt_auth).read(32)
 
     nonce = convert[-16:]
 
@@ -91,5 +98,3 @@ def decrypt_password(password_to_decrypt, master_password_hash):
     plaintext = cipher.decrypt(convert[:-16]) 
 
     return plaintext
-
-
